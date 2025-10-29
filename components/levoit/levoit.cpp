1605eb5229300e651cb0ca29d9145634130e717e
ChriSoftIO <62946133+ChriSoftIO@users.noreply.github.com>
2025-10-29 22:07:07 +0100

    t

--- components/levoit/levoit.cpp
+++ components/levoit/levoit.cpp
@@ -208,28 +208,28 @@ void Levoit::command_sync_() {
         send_command_(LevoitCommand {
           .payloadType = LevoitPayloadType::SET_FAN_MANUAL,
           .packetType = LevoitPacketType::SEND_MESSAGE,
-          .payload = {0x00, 0x00, 0x01, 0x01},
+          .payload = {0x00, 0x00, 0x00, 0x00},
           .payload_len = 4
         });
       } else if (req_on_state_ & static_cast<uint32_t>(LevoitState::FAN_SPEED2)) {
         send_command_(LevoitCommand {
           .payloadType = LevoitPayloadType::SET_FAN_MANUAL,
           .packetType = LevoitPacketType::SEND_MESSAGE,
-          .payload = {0x00, 0x00, 0x01, 0x02},
+          .payload = {0x00, 0x00, 0x01, 0x01},
           .payload_len = 4
         });
       } else if (req_on_state_ & static_cast<uint32_t>(LevoitState::FAN_SPEED3)) {
         send_command_(LevoitCommand {
           .payloadType = LevoitPayloadType::SET_FAN_MANUAL,
           .packetType = LevoitPacketType::SEND_MESSAGE,
-          .payload = {0x00, 0x00, 0x01, 0x03},
+          .payload = {0x00, 0x00, 0x01, 0x05},
           .payload_len = 4
         });
       } else if (req_on_state_ & static_cast<uint32_t>(LevoitState::FAN_SPEED4)) {
         send_command_(LevoitCommand {
           .payloadType = LevoitPayloadType::SET_FAN_MANUAL,
           .packetType = LevoitPacketType::SEND_MESSAGE,
-          .payload = {0x00, 0x00, 0x01, 0x04},
+          .payload = {0x00, 0x00, 0x01, 0x09},
           .payload_len = 4
         });
       }
