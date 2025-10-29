70b47d2352cb897a75cca321dc82a2143aff5665
ChriSoftIO <62946133+ChriSoftIO@users.noreply.github.com>
2025-10-29 21:47:27 +0100

    t

--- components/levoit/sensor/__init__.py
+++ components/levoit/sensor/__init__.py
@@ -9,7 +9,8 @@ from esphome.const import (
     DEVICE_CLASS_PM25,
     DEVICE_CLASS_AQI,
     STATE_CLASS_MEASUREMENT,
-    DEVICE_CLASS_HUMIDITY
+    DEVICE_CLASS_HUMIDITY,
+    UNIT_PERCENT
 )
 
 from .. import levoit_ns, CONF_LEVOIT_ID, Levoit
@@ -27,7 +28,7 @@ CONFIG_SCHEMA = (
         cv.GenerateID(CONF_LEVOIT_ID): cv.use_id(Levoit),
         cv.Optional(CONF_PM_2_5): sensor.sensor_schema(LevoitSensor, unit_of_measurement=UNIT_MICROGRAMS_PER_CUBIC_METER, icon=ICON_BLUR, accuracy_decimals=1, device_class=DEVICE_CLASS_PM25, state_class=STATE_CLASS_MEASUREMENT),
         cv.Optional(CONF_AIR_QUALITY): sensor.sensor_schema(LevoitSensor, icon=ICON_BLUR, accuracy_decimals=0, device_class=DEVICE_CLASS_AQI, state_class=STATE_CLASS_MEASUREMENT),
-        cv.Optional("humidity"): sensor.sensor_schema(LevoitSensor, accuracy_decimals=0, device_class=DEVICE_CLASS_HUMIDITY, state_class=STATE_CLASS_MEASUREMENT)
+        cv.Optional("humidity"): sensor.sensor_schema(LevoitSensor, unit_of_measurement=UNIT_PERCENT, accuracy_decimals=0, device_class=DEVICE_CLASS_HUMIDITY, state_class=STATE_CLASS_MEASUREMENT)
     })
 )
 
