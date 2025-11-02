495034f68ceda0aaf6e75833869d19109e22791f
ChriSoftIO <62946133+ChriSoftIO@users.noreply.github.com>
2025-11-02 16:32:33 +0100

    t

---
+++ components/levoit/binary_sensor/levoit_binary_sensor.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/levoit/levoit.h"
+#include "esphome/components/binary_sensor/binary_sensor.h"
+
+namespace esphome {
+namespace levoit {
+
+
+class LevoitSensor : public Component, public binary_sensor::BinarySensor {
+ public:
+  void set_button(binary_sensor::BinarySensor* button) {
+    this->button_ = button;
+  }
+ protected:
+  Levoit *parent_;
+  LevoitSensorPurpose purpose_;
+
+  binary_sensor::BinarySensor* button_{nullptr};
+};
+
+}  // namespace levoit
+}  // namespace esphome
