639051fa2a1e017fe5cd77902ec3a26d7ea8a189
ChriSoftIO <62946133+ChriSoftIO@users.noreply.github.com>
2025-11-02 16:28:16 +0100

    t

--- components/levoit/sensor/levoit_sensor.h
+++ components/levoit/sensor/levoit_sensor.h
@@ -3,27 +3,21 @@
 #include "esphome/core/component.h"
 #include "esphome/components/levoit/levoit.h"
 #include "esphome/components/sensor/sensor.h"
-#include "esphome/components/binary_sensor/binary_sensor.h"
 
 namespace esphome {
 namespace levoit {
 
 enum LevoitSensorPurpose : uint8_t { PM25, AIR_QUALITY, HUMIDITY };
 
-class LevoitSensor : public Component{
+class LevoitSensor : public Component, public sensor::Sensor {
  public:
   LevoitSensor(Levoit *parent, LevoitSensorPurpose purpose) : parent_(parent), purpose_(purpose) {}
   void setup() override;
   void dump_config() override;
 
-  void set_button(binary_sensor::BinarySensor* button) {
-    this->button_ = button;
-  }
-
  protected:
   Levoit *parent_;
   LevoitSensorPurpose purpose_;
-  binary_sensor::BinarySensor* button_{nullptr};
 };
 
 }  // namespace levoit
